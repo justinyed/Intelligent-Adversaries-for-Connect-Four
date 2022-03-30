65c29042aa64d8fbdd71c98b95fdbc55110a78b7
Justin Yedinak <justin.yedinak.19@cnu.edu>
2022-03-30 11:11:13 -0400

    RL

--- intelligence/reinforcement_learning_agent.py
+++ intelligence/reinforcement_learning_agent.py
@@ -1,13 +1,18 @@
 from abc import ABC
 from collections import defaultdict
 import random
+
+import numpy as np
+
 import intelligence.learning_agent as learning
 import intelligence.agent as agent
 import intelligence.search_agents as sa
 import game_components.connect_four as c4
 import intelligence.successor_generator as gen
+import intelligence.evaluation_functions as eval_fn
 import os
 import json
+
 PLAYER2 = -1
 WINNING_VALUE = 1000000
 LOSING_VALUE = -1000000
@@ -122,7 +127,8 @@ class QLearning(Reinforcement):
     Q-Learning Agent
     """
 
-    def __init__(self, values=None, learning_rate: float = 1.0, exploration_rate: float = 0.05, discount_factor: float = 0.8,
+    def __init__(self, values=None, learning_rate: float = 1.0, exploration_rate: float = 0.05,
+                 discount_factor: float = 0.8,
                  iterations: int = 10):
         """
         You can initialize Q-values here...
@@ -133,11 +139,14 @@ class QLearning(Reinforcement):
         else:
             self.values = values
 
+        self.unseen_states = 0
+        self.seen_states = 0
+
     def get_q_value(self, state, action):
         """
         Returns Q(state,action), otherwise 0.0 if state has never been seen.
         """
-        return self.values[hash(state) ^ hash(action)]
+        return self.values[hash((state, action))]
 
     def compute_value_from_q_values(self, state):
         """
@@ -166,6 +175,12 @@ class QLearning(Reinforcement):
 
         for action in state.get_legal_actions():
             q = self.get_q_value(state, action)
+
+            if q == 0.0:
+                self.unseen_states += 1
+            else:
+                self.seen_states += 1
+
             if value < q:
                 best_action = action
                 value = q
@@ -186,14 +201,21 @@ class QLearning(Reinforcement):
             action = self.get_policy(state)
         return action
 
-    # def _neutralize_state(self, state):
-    #     if self._player == PLAYER2:
-    #         board, turn, status = state.get_state()
-    #         grid = board.get_grid()
-    #         grid *= -1
-    #         board.set_grid(grid)
-    #         state = c4.ConnectFour(state=(board, turn, status))
-    #     return state
+    def _neutralize_state(self, state):
+        if self._player == PLAYER2:
+            board, turn, status = state.get_state()
+            grid = board.get_grid()
+            grid *= -1
+            board.set_grid(grid)
+            state = c4.ConnectFour(state=(board, turn, status))
+        return state
+
+    @staticmethod
+    def get_mirror_state(state):
+        board, turn, status = state.get_state()
+        grid = board.get_grid()
+        board.set_grid(np.fliplr(grid))
+        return c4.ConnectFour(state=(board, turn, status))
 
     def update(self, state, action, next_state, reward):
         """
@@ -201,8 +223,14 @@ class QLearning(Reinforcement):
         """
 
         sample = reward + self.discount_factor * self.compute_value_from_q_values(next_state)
-        self.values[hash(state) ^ hash(action)] = (1.0 - self.learning_rate) * self.get_q_value(state,
-                                                                                     action) + self.learning_rate * sample
+        self.values[hash((state, action))] = (1.0 - self.learning_rate) * self.get_q_value(state,
+                                                                                           action) + self.learning_rate * sample
+
+    def get_unseen(self):
+        return self.unseen_states
+
+    def get_seen(self):
+        return self.seen_states
 
     def get_policy(self, state):
         return self.compute_action_from_q_values(state)
@@ -211,8 +239,8 @@ class QLearning(Reinforcement):
         return self.compute_value_from_q_values(state)
 
     @staticmethod
-    def train(opponent, learning_rate: float = 1.0, exploration_rate: float = 0.05, discount_factor: float = 0.8,
-              iterations: int = 10, reward_function=reward_function):
+    def train(opponent, learning_rate: float = 1.0, exploration_rate: float = 0.05, discount_factor: float = 0.7,
+              iterations: int = 10, reward_function=eval_fn.evaluation_function_weighted_matrix):
         """ todo
         :param opponent:
         :param learning_rate:
@@ -251,18 +279,22 @@ class QLearning(Reinforcement):
                 reward = reward_function(next_state, learner.get_player())
 
                 learner.observe_transition(state, action, next_state, reward)
+                learner.observe_transition(QLearning.get_mirror_state(state), abs(action - 6), QLearning.get_mirror_state(next_state), reward)
 
                 state = next_state
 
             print(f"\tt={state.get_turn():03d}; s={state.get_status()}")
             learner.stop_episode()
 
-
         return learner.values
 
 
 if __name__ == '__main__':
-    values = QLearning.train(sa.AlphaBeta(depth_limit=1), iterations=1000, exploration_rate=.60, learning_rate=0.0001, discount_factor=.9)
+    values = QLearning.train(sa.MiniMax(depth_limit=1),
+                             iterations=10000,
+                             exploration_rate=0.70,
+                             learning_rate=0.001,
+                             discount_factor=0.90)
     file = "./data/values.json"
 
     try:
@@ -275,6 +307,3 @@ if __name__ == '__main__':
 
     except IOError:
         print("IO Error")
-
-
-
